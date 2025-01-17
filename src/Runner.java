aba88bfbb81fe0b188d4043fcac54d7b937a1c61
Aastapchik <154362326+Aastapchik@users.noreply.github.com>
2025-01-17 16:01:13 +0300

    Update Runner.java
    Теперь работает с пробелами

--- src/Runner.java
+++ src/Runner.java
@@ -4,7 +4,7 @@ public class Runner {
 
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
-        String s = scanner.nextLine();
+        String s = scanner.nextLine().replace(" ", "");
         System.out.println(Calculator.calc(s));
     }
 }
